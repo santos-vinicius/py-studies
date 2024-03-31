667f277d7fdc27b8f6aa296b06330077b89b4e11
Vinicius Santos <santosviniv@gmail.com>
2024-03-30 22:59:05 -0300

    🌱 manipulação de strings

---
+++ otavio-miranda/manipulacao-strings.py
@@ -0,0 +1,19 @@
+"""
+Manipulando strings
+* String indices
+* Fatiamento de strings [inicio:fim:passo]
+* Funções built-in len, abs, type, print, etc...
+Essas funções podem ser usadas diretamente em casa tipo.
+
+Mais informações:
+https://docs.python.org/3/library/stdtypes.html (tipos built-in)
+https://docs.python.org/3/library/functions.html (funções built-in)
+"""
+
+texto = 'Python s2'
+
+# acessando índice específico
+print(texto[2])
+
+nova_string = texto[0:6]
+print(nova_string)
