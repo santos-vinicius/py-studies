43cf198fb545c9e394df3d519c2a8533890e8521
Vinicius Santos <santosviniv@gmail.com>
2024-04-17 21:35:59 -0300

    imc exercice

---
+++ exercices/imc.py
@@ -0,0 +1,8 @@
+nome = 'Vinicius Santos'
+altura = 1.85
+peso = 72
+imc = peso / altura ** 2  # ... (ELLIPSIS =  PLACEHOLDER) # IMC = peso // (altura * altura)
+
+
+print(f'{nome} tem {altura} de altura')
+print(f'Pesa {peso} quilos e seu IMC é {imc:.2f}')
