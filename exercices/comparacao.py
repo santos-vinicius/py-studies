abb7fe03589aa5810e8ba06797d5b84e3c70acc4
Vinicius Santos <santosviniv@gmail.com>
2024-04-21 21:15:06 -0300

    exercicio de comparacao - OM

---
+++ exercices/comparacao.py
@@ -0,0 +1,9 @@
+primeiro_valor = int(input('Digite um valor: '))
+segundo_valor = int(input('Digite outro valor: '))
+
+# comparação de valores
+
+if primeiro_valor >= segundo_valor:
+  print(f'O primeiro valor: "{primeiro_valor}" é maior ou igual que o segundo valor: "{segundo_valor}".')
+else:
+  print(f'O segundo valor: "{segundo_valor}" é maior ou igual que o primeiro valor: "{primeiro_valor}".')
